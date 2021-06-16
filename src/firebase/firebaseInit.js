803abb96860ad87962a23b8b7f0615a35e8440a3
Idris Khan <idris.khan303@gmail.com>
2021-06-16 15:10:09 +0100

    initial commit

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVuznhIn--O1sVPF5LhdFs0yqXPM3zHvI',
+  authDomain: 'fireblogs-9a966.firebaseapp.com',
+  projectId: 'fireblogs-9a966',
+  storageBucket: 'fireblogs-9a966.appspot.com',
+  messagingSenderId: '539709133135',
+  appId: '1:539709133135:web:390a90bf5f8edf0430f53a',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
